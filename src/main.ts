fb77dcaf1f3e3e1f01342006c4c6a1c3e8984c78
Vladimir <vladimircuriel@outlook.com>
2024-01-07 16:28:53 -0400

    Fixed Port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
   app.disable('x-powered-by');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
